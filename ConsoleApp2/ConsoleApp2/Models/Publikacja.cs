33211a725f8ad90cf4c9ef35b797ca374c05c3f7
murvudd <32345616+murvudd@users.noreply.github.com>
2017-10-16 20:49:42 +0200

    Lab2, not ended

---
+++ ConsoleApp2/ConsoleApp2/Models/Publikacja.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.Models
+{
+    class Publikacja
+    {
+        public int ID { get; set; }
+        public string Autorzy { get; set; }
+        public string Tytuł { get; set; }
+        public int RokWydania { get; set; }
+         
+
+    }
+}
