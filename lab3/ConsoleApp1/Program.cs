c44505c347c088c84f39cbf4b623a1c16364aeec
murvudd <oddajcieminick@gmail.com>
2017-10-24 12:08:39 +0200

    1

--- lab3/ConsoleApp1/Program.cs
+++ lab3/ConsoleApp1/Program.cs
@@ -3,79 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace ConsoleApp1
+namespace Lab3
 {
+    static class Parse
+    {
+        public static int? ToNullableInt(this string s)
+        {
+            int i;
+            if (int.TryParse(s, out i)) return i;
+            return null;
+        }
+    }
+    class Case  {
+        public int? Year {get; set;}
+        public int? Age { get; set; }
+        public string Type { get; set; }
+        public string Country { get; set; }
+        public string Name { get; set; }
+        public string Area { get; set; }
+        public string Activity { get; set; }
+        public string Sex { get; set; }
+        public string Fatal { get; set; }
+    }
     class Program
     {
-        class DepartmentClass
-        {
-            public int DepartmentId { get; set; }
-            public string Name { get; set; }
-        }
-        class EmployeeClass
-        {
-            public int EmployeeId { get; set; }
-            public string EmployeeName { get; set; }
-            public int DepartmentId { get; set; }
-            public int YeartoRetitement { get; set; }
-        }
-        
         static void Main(string[] args)
         {
-            //lista obiektow opisujacych dzial
-            List<DepartmentClass> departments = new List<DepartmentClass>();
-            departments.Add(new DepartmentClass { DepartmentId = 1, Name = "Accounting" });
-            departments.Add(new DepartmentClass { DepartmentId = 2, Name = "Sales" });
-            departments.Add(new DepartmentClass { DepartmentId = 3, Name = "Marketing" });
-            //lista obiektow pracownikow z id dzialu
-            List<EmployeeClass> employees = new List<EmployeeClass>();
-            employees.Add(new EmployeeClass { DepartmentId = 1, EmployeeId = 1, EmployeeName = "William", YeartoRetitement = 40 });
-            employees.Add(new EmployeeClass { DepartmentId = 2, EmployeeId = 2, EmployeeName = "Miley", YeartoRetitement = 5 });
-            employees.Add(new EmployeeClass { DepartmentId = 1, EmployeeId = 3, EmployeeName = "Benjamin", YeartoRetitement = 30 });
+            List<Case> CaseList = new List<Case>();
 
-            // przykład składni
-            // string[] wierszyk = { "a", "bc", "def", "gihjk","kdkd","sad", "gk" };
-            // var lista = from w in wierszyk where (w.Length < 4) select w;
-            // var lista = wierszyk.Where(s => s.Length < 4); // składnia lambda równoważna linijce wyżej
-            //foreach(var w in lista)
-            //{
-            //    Console.WriteLine(w);
-            //}
+            string[] plik = File.ReadAllLines("attacks.csv");
+            //char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
+            for (int i=1; i<plik.Length;i++) {
+                string[] b = plik[i].Split(';');
+                CaseList.Add(new Case { Year = Parse.ToNullableInt(b[2]),
+                    Type = b[3], Country = b[4], Area = b[5], Activity = b[7],
+                    Name = b[8], Sex = b[9], Age = Parse.ToNullableInt (b[10]) });
 
-            //var list = (from e in employees where (e.DepartmentId == 1) select e);
-            // var list = employees.Where(el => el.DepartmentId ==1);
-            // var list1 = employees.Average(el => el.DepartmentId ==1);
-            //foreach (var e in list)
-            //{
-            //    Console.WriteLine(e.EmployeeName);
-            //}
-            //var list1 = from e in employees join d in departments on e.DepartmentId // łączenie dwóch list w nową listę 
-            //            equals d.DepartmentId select new { EN = e.EmployeeName, DN = d.Name };
-            //foreach(var el in list1)
-            //{
-            //    Console.WriteLine("{0} works in {1}", el.EN, el.DN);
-            //}
+            }
 
-            // grupowanie
-
-            //IEnumerable<IGrouping<int, EmployeeClass>>
-            //    list = from element in employees
-            //           group element by element.YeartoRetitement
-            //           into empgroup orderby empgroup.Key select empgroup;
-
-            //foreach(var group in list)
-            //{
-            //    foreach (EmployeeClass e in group)
-            //    {
-            //        Console.WriteLine(group.Key + " " + e.EmployeeName);
-            //    }
-            //}
+            //var list = (from e in CaseList where ((e.Country == "USA") && (e.Area == "Florida")) select e);
+            var list = from e in CaseList
+                       where e.Country == "USA" && e.Area =="Florida"
+                       select e;
 
 
-            var list = (from e in employees where (e.DepartmentId == 1) select e.YeartoRetitement).Average();
-            Console.WriteLine("{0}       {1}", list, list.GetType());
-
+            foreach (var w in list)
+            {
+                Console.WriteLine(w.Area);
+            }
+            Console.WriteLine(list.Count());
+            
+            
         }
     }
 }
--- mysql_connect/mysql_connect/Program.cs
+++ mysql_connect/mysql_connect/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using mysql_connect.Models;
+using System.Diagnostics;
 
 namespace mysql_connect
 {
@@ -17,15 +18,20 @@ namespace mysql_connect
             {
                 try
                 {
+                    Stopwatch timer = new Stopwatch();
                     Random val = new Random();
                     //a.Insert("Create table logs(temp double, czas datetime(6) not null primary key) engine = myisam; ");
                     //a.Insert("");
+                    timer.Start();
                     for (int i = 0; i < 1000; i++)
                     {
                         a.Insert("insert into   lab4.logs"+j+"(temp, czas) Values (" 
                                             + val.Next(0, 10000000) + ", now(6)); ");
                     }
-                    Console.WriteLine("logs{1} query passed   {0} rows", a.Count("select count(*) from logs"+j),j);
+                    timer.Stop();
+                    TimeSpan ts = timer.Elapsed;
+                    Console.WriteLine("logs{1} query passed   {0} rows       {2} time", a.Count("select count(*) from logs"+j),j,ts);
+                    
                 }
 
                 catch (Exception e)
