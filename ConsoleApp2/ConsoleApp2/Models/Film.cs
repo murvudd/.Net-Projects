1b668376558899dfc38a3e5efdf9f784178e757c
murvudd <oddajcieminick@gmail.com>
2017-11-05 15:51:46 +0100

    5.11.17 recoverylab2

---
+++ ConsoleApp2/ConsoleApp2/Models/Film.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Lab2.Models
+{
+    class Film : Multimedia
+    {
+        public int DługośćFilmu { get; set; }
+
+        public Film(int DF, string rodzaj, string autorzy, string tytul, int rok) : base(rodzaj, autorzy, tytul, rok)
+        {
+            this.DługośćFilmu = DF;
+        }
+    }
+}
