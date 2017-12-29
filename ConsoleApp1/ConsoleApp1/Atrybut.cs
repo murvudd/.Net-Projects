7286ea9338815a60b2b011999bf052276c27d12c
murvudd <oddajcieminick@gmail.com>
2017-12-30 00:23:04 +0100

    2

---
+++ ConsoleApp1/ConsoleApp1/Atrybut.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Atrybut
+    {
+        public string a { get; set; }
+        public string b { get; set; }
+        public string c { get; set; }
+        public string d { get; set; }
+        public string e { get; set; }
+        public string f { get; set; }
+
+        //public Atrybut(string _a, string _b, string _c, string _d, string _e, string _f)
+        //{
+        //    a = _a;
+        //    b = _b;
+        //    c = _c;
+        //    d = _d;
+        //    e = _e;
+        //    f = _f;
+        //}
+        //public int IleWystąpień(string x,int sw )
+        //{
+        //    int i = 0;
+            
+        //    switch (sw)
+        //    {
+        //        case 1:
+        //            this.a;
+        //            break;
+        //        case 2:
+        //            this.b
+        //            break;
+        //        case 3:
+        //            this.c
+        //            break;
+        //        case 4:
+        //            this.d
+        //            break;
+        //        case 5:
+        //            this.e
+        //            break;
+        //        case 6:
+        //            this.f
+        //            break;
+        //    }
+        //    return i;
+        //}
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,13 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace ConsoleApp
+
+namespace ConsoleApp1
 {
+    class Parse
+    {
+        static bool StringToBool(string x)
+        {
+            if(x == "1") return true;
+            
+            else return false;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            List<Branch> ListaOpcji = new List<Branch>();
+            List<Atrybut> ListaA = new List<Atrybut>();
+            ReadPlik(ListaA);
+            Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t", ListaA[0].a, ListaA[0].b, ListaA[0].c, ListaA[0].d, ListaA[0].e, ListaA[0].f);            
+
+
+        }
+        static int IleWystąpień(List<Atrybut> ListaA)
+        {
+            int i = 0;
+
+            return i;
+        }
+
+        static void ReadPlik(List<Atrybut> ListaA)
+        {
+            string[] plik = File.ReadAllLines("C:\\Users\\Żaba\\Desktop\\plik1.txt");
+            for (int i = 0; i < plik.Length; i++)
+            {
+                string[] line = plik[i].Split(' ');
+                ListaA.Add(new Atrybut
+                {
+                    a = line[0],
+                    b = line[1],
+                    c = line[2],
+                    d = line[3],
+                    e = line[4],
+                    f = line[5],
+                });
+            }
+
         }
     }
 }
