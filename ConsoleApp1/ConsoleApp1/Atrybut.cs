620086797f0df67b3e7fc5b06350b485a7852c48
murvudd_PC <oddajcieminick@gmail.com>
2018-01-02 19:50:52 +0100

    3

--- ConsoleApp1/ConsoleApp1/Atrybut.cs
+++ ConsoleApp1/ConsoleApp1/Atrybut.cs
@@ -29,6 +29,33 @@ namespace ConsoleApp1
         //    e = _e;
         //    f = _f;
         //}
+
+        public List<string> IleWystąpień(char sw)
+        {
+            string[] v = { " ", " " };
+            List<string> lista = new List<string>();
+            switch (sw)
+            {
+                case 'a':
+                    if() lista.Add(this.a);
+                    break;
+                case 'b':
+                    
+                    break;
+                case 'c':
+                    
+                    break;
+                case 'd':
+                    
+                    break;
+                case 'e':
+                    
+                    break;
+            }
+
+            return lista;
+        }
+
         public int IleWystąpień(char sw, string x)
         {
             int i = 0;
@@ -68,5 +95,47 @@ namespace ConsoleApp1
 
             return i;
         }
+
+        public int IleWystąpień(char sw, string x, bool B)
+        {
+            int i = 0;
+            switch (sw)
+            {
+                case 'a':
+                    for (int k = 0; k < 8; k++)
+                    {
+                        if (this.a == x && Convert.ToBoolean(int.Parse(this.f)) == B) i++;
+                    }
+                    break;
+                case 'b':
+                    for (int k = 0; k < 8; k++)
+                    {
+                        if (this.b == x && Convert.ToBoolean(int.Parse(this.f)) == B) i++;
+                    }
+                    break;
+                case 'c':
+                    for (int k = 0; k < 8; k++)
+                    {
+                        if (this.c == x && Convert.ToBoolean(int.Parse(this.f)) == B) i++;
+                    }
+                    break;
+                case 'd':
+                    for (int k = 0; k < 8; k++)
+                    {
+                        if (this.d == x && Convert.ToBoolean(int.Parse(this.f)) == B) i++;
+                    }
+                    break;
+                case 'e':
+                    for (int k = 0; k < 8; k++)
+                    {
+                        if (this.e == x && Convert.ToBoolean(int.Parse(this.f)) == B) i++;
+                    }
+                    break;
+            }
+
+            return i;
+        }
+
+        
     }
 }
