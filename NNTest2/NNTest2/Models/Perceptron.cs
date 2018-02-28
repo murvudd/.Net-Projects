db25a4bf94ead187e8e33bbbe9c34de00fb1dd48
murvudd_PC <oddajcieminick@gmail.com>
2018-03-01 00:44:58 +0100

    1

---
+++ NNTest2/NNTest2/Models/Perceptron.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace test2.Models
+{
+    class Perceptron
+    {
+        //protected double[] Inputs { get; set; }
+        public double[] Inputs { get; set; }
+        //protected double[] Weights { get; set; }
+        public double[] Weights { get; set; }
+
+        public Perceptron(double[] inp)
+        {
+            var rng = new Random();
+            Inputs = inp;
+            var W = new double[inp.Count()];
+            for (int i = 0; i < inp.Count(); i++)
+            {
+                W[i] = rng.Next(-1000, 1001) * 0.001;
+                Thread.Sleep(1);
+            }
+            Weights = W;
+        }
+
+        public void Print()
+        {
+            int i = 0;
+            foreach (var item in this.Inputs)
+            {
+                i += 1;
+                Console.WriteLine("input no. {0} is {1} with weight {2} ", i, item, this.Weights[i - 1]);
+            }
+        }
+
+        public int Guess()
+        {
+            double sum = 0;
+            for (int i = 0; i < Inputs.Count(); i++)
+            {
+                sum += Inputs[i] * Weights[i];
+            }
+            return Math.Sign(sum);
+        }
+
+    }
+}
---
+++ NNTest2/NNTest2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using test2.Models;
+
+namespace NNtest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var a = new Perceptron(new double[] { });
+            var b = new Perceptron(new double[] { 1, 2, 3 });
+            var c = new Perceptron(new double[] { 2, 3, 4, 5 });
+            var d = new Perceptron(new double[] { 3, 4, 5, 6, 7 });
+
+            d.Print();
+
+            Console.WriteLine(d.Guess());
+
+
+            Console.ReadLine();
+        }
+    }
+}
