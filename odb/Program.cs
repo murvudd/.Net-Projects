1a7f54ac64ae016f05d2bfeadb211f3670a6c4ab
murvudd <oddajcieminick@gmail.com>
2018-03-28 11:36:52 +0200

    OracleConnetor done

--- odb/Program.cs
+++ odb/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+   
 
 namespace odb
 {
@@ -11,74 +13,66 @@ namespace odb
         static void Main(string[] args)
         {
             MyConnect a = new MyConnect();
-            try
-            {
-            a.Insert("");
-
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
-
-
-        }
-
-        public static string RNGCity()
-        {
-            string s;
-            Random rng = new Random();
-
-            switch (rng.Next(0,10))
-            {
-                case 0:
-                    s = "Warszawa";
-                    break;
-
-                case 1:
-                    s = "Kraków";
-                    break;
-
-                case 2:
-                    s = "Łódź";
-                    break;
-
-                case 3:
-                    s = "Wrocław";
-                    break;
-
-                case 4:
-                    s = "Poznań";
-                    break;
-
-                case 5:
-                    s = "Gdańsk";
-                    break;
-
-                case 6:
-                    s = "Szczecin";
-                    break;
-
-                case 7:
-                    s = "Bydgoszcz";
-                    break;
-
-                case 8:
-                    s = "Lublin";
-                    break;
-
-                case 9:
-                    s = "Katowice";
-                    break;
-
-                default:
-                    s = "Internet";
-                    break;
-            }
-            return s;
+            OracleConnect b = new OracleConnect();
             
+            b.Check();
+
         }
+
+        //public static string RNGCity()
+        //{
+        //    string s;
+        //    Random rng = new Random();
+
+        //    switch (rng.Next(0,10))
+        //    {
+        //        case 0:
+        //            s = "Warszawa";
+        //            break;
+
+        //        case 1:
+        //            s = "Kraków";
+        //            break;
+
+        //        case 2:
+        //            s = "Łódź";
+        //            break;
+
+        //        case 3:
+        //            s = "Wrocław";
+        //            break;
+
+        //        case 4:
+        //            s = "Poznań";
+        //            break;
+
+        //        case 5:
+        //            s = "Gdańsk";
+        //            break;
+
+        //        case 6:
+        //            s = "Szczecin";
+        //            break;
+
+        //        case 7:
+        //            s = "Bydgoszcz";
+        //            break;
+
+        //        case 8:
+        //            s = "Lublin";
+        //            break;
+
+        //        case 9:
+        //            s = "Katowice";
+        //            break;
+
+        //        default:
+        //            s = "Internet";
+        //            break;
+        //    }
+        //    return s;
+            
+        //}
 
 
     }
