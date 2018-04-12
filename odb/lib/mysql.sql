82cd5a5fe137986fc4f7c06392d0598ab9727f89
murvudd_PC <oddajcieminick@gmail.com>
2018-04-12 03:12:17 +0200

    1

--- odb/lib/mysql.sql
+++ odb/lib/mysql.sql
@@ -5,11 +5,13 @@ order by TABLE_NAME;
 
 insert into shops (city) values ('Warszawa');
 select * from shops order by shop_id;
-select * from stock ;
+select * from stock;
+select * from customers order by customer_id;
+select count(*) from customers;
 truncate table shops;
 
 -- SELECT (data_length+index_length)/power(1024,3) tablesize
-SELECT (data_length+index_length)/power(1024,2) tablesize
+SELECT (data_length+index_length)/power(1024,3) tablesize
 FROM information_schema.tables
 WHERE table_schema='eshopinnodb' and table_name='customers';
 
